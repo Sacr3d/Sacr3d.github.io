2cd4ae6de15e5e834d8144c3d22f3087aea446ed
Mattia Sassone <11msassone99@gmail.com>
2022-02-10 13:14:45 +0000

    TAXII Discovery changed to SL TAXII Discovery

--- src/views/Discovery/index.tsx
+++ src/views/Discovery/index.tsx
@@ -5,13 +5,11 @@ export default function DiscoveryView() {
 	return (
 		<>
 			<Header
-				title="TAXII Discovery"
+				title="SL TAXII Discovery"
 			/>
 			<main>
 				<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-					{/* Replace with your content */}
 					<DiscoveryComponent />
-					{/* /End replace */}
 				</div>
 			</main>
 		</>
